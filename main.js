778e6b12409b6005e11c61b7db7ac1cfad03d589
mehedi rana <mdmehedirana@gmail.com>
2020-06-26 18:23:15 +0600

    3rd

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ let firstName = prompt("Enter Your Name: ");
 console.log(firstName)
 let secName = prompt("Enter your parthner Name: ");
 console.log(secName)
-let result = Math.random() * 100
-console.log(result)
+let result = Math.random() * 100;
+let newV = Math.floor(result)
+console.log(newV)
+alert("your love %percentage% is: " + newV + "%")
