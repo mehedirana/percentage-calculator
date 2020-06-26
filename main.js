df828b066c1c2dd5071b4a8d241cc31f8cfdef20
mehedi rana <mdmehedirana@gmail.com>
2020-06-26 18:18:20 +0600

    2nd

---
+++ main.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Enter Your Name: ");
+console.log(firstName)
+let secName = prompt("Enter your parthner Name: ");
+console.log(secName)
+let result = Math.random() * 100
+console.log(result)
